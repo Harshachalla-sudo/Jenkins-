7f366813c5d079cc4d103ea81291663c34752b75
Harshachalla-sudo <harshachallac@gmail.com>
2026-06-22 10:15:48 +0530

    Update world.java

--- world.java
+++ world.java
@@ -1,5 +1,9 @@
-public class world {
+public class Table {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int num = 5;
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
     }
 }
