ddb0e87d4ff78d60f6c05d7879aaaf903af181dc
Harshachalla-sudo <harshachallac@gmail.com>
2026-06-22 10:27:52 +0530

    Update world.java

--- world.java
+++ world.java
@@ -1,6 +1,6 @@
 public class world {
     public static void main(String[] args) {
-        int num = 5;
+        int num = 6;
 
         for (int i = 1; i <= 10; i++) {
             System.out.println(num + " x " + i + " = " + (num * i));
